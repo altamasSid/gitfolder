3d354dc7f6621a50a1e1b877c189e071423448b5
altamasSid <altamas55@gmail.com>
2023-06-15 15:42:23 +0530

    added message in sum.java file

--- sum.java
+++ sum.java
@@ -1,10 +1,10 @@
-class sum{
+public class sum{
 
 
     public static void main(String[] args){
         int a=2;
         int b=8;
-        System.out.println(a+b);
+        System.out.println("The sum is "+(a+b));
     }
 
 }
